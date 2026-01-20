7ccbd9ed1e85a0ca000f24b910fd81c0ff97a32f
fly <134721942+flying-go@users.noreply.github.com>
2026-01-20 19:08:19 +0800

    Update index.js

--- projects/biconomy-cex/index.js
+++ projects/biconomy-cex/index.js
@@ -10,19 +10,6 @@ const config = {
        '0xAd8E5cEb7D77e10403Be8430717c515273c31b8d'
     ],
   },
-  bitcoin: {
-    owners: bitcoinAddressBook.biconomy
-  },
-  polygon: {
-    owners: [
-        '0x366ba28Ec89113454EA6e82bB606426e8cA22780'
-    ],
-  },
-  tron: {
-    owners: [
-        'TEi2hVWDRMo61PAoi1Dwbn8hNXufkwEVyp'
-    ]
-  },
 }
 
 module.exports = cexExports(config)
